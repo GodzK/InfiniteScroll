8ec103ddda411c1679a35caf78f4d2947956b81b
Phakaphol Dherachaisuphakij <64467457+GodzK@users.noreply.github.com>
2024-04-25 19:53:03 +0700

    fix the code

--- infinitescroll/src/components/PhotoComponents.jsx
+++ infinitescroll/src/components/PhotoComponents.jsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import './PhotoComponents.css'
 
 const PhotoComponent = ({ title, description, url, types, topics, levels }) => {
+  const handleClick = () => {
+    window.location.href = url;
+  };
   return (
         <div className="card">
     <div className="card-header"><h1>{description}</h1></div>
@@ -10,8 +13,8 @@ const PhotoComponent = ({ title, description, url, types, topics, levels }) => {
         <h2>Types: {types.join(', ')}</h2>
         <h2>Topics: {topics.join(', ')}</h2>
         <h2>Levels : {levels.join(', ')}</h2>
-        <button className='button-86' >
-            Learn More
+        <button className='button-86' onClick={handleClick}>
+              Learn More
           </button>
     </div>
 </div>
